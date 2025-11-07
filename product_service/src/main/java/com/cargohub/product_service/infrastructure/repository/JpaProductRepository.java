e2a272f15a45eb9bbca13ce82b1ec4215cc19d43
ur <dorosi150@gmail.com>
2025-11-07 15:43:29 +0900

    feat: ProductRepository 추가

---
+++ product_service/src/main/java/com/cargohub/product_service/infrastructure/repository/JpaProductRepository.java
@@ -0,0 +1,9 @@
+package com.cargohub.product_service.infrastructure.repository;
+
+import com.cargohub.product_service.domain.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface JpaProductRepository extends JpaRepository<Product, UUID> {
+}
---
+++ product_service/src/main/java/com/cargohub/product_service/infrastructure/repository/ProductRepositoryAdaptor.java
@@ -0,0 +1,12 @@
+package com.cargohub.product_service.infrastructure.repository;
+
+
+import com.cargohub.product_service.domain.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ProductRepositoryAdaptor implements ProductRepository {
+
+}
