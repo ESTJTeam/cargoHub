1872cc062ccff733bbc572f9aa06fb1a083a66d1
Jess <jcscene@gmail.com>
2025-11-10 12:32:49 +0900

    chore: slack 모듈 생성

---
+++ slack_service/src/main/java/slack_service/slack_service/SlackServiceApplication.java
@@ -0,0 +1,13 @@
+package slack_service.slack_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackServiceApplication.class, args);
+    }
+
+}
