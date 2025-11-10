1763d7264b25c8e61da3938ed54cd604b7ac8fa6
sunwoo <kgw6147@naver.com>
2025-11-10 13:48:31 +0900

    [FEAT] 허브 Repo Adaptor 생성

---
+++ hub_service/src/main/java/hub_server/hub_server/infra/repository/HubInfoRepositoryAdaptor.java
@@ -0,0 +1,6 @@
+package hub_server.hub_server.infra.repository;
+
+import hub_server.hub_server.domain.repository.HubInfoRepository;
+
+public class HubInfoRepositoryAdaptor implements HubInfoRepository {
+}
