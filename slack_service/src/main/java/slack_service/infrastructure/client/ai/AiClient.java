ba6bbb3e5a7cc523ab87ddddfea796fa800913ba
Jess <jcscene@gmail.com>
2025-11-12 19:03:32 +0900

    [FIX] AiClient에 url 바인딩 안 되는 문제 해결

--- slack_service/src/main/java/slack_service/infrastructure/client/ai/AiClient.java
+++ slack_service/src/main/java/slack_service/infrastructure/client/ai/AiClient.java
@@ -1,5 +1,18 @@
 package slack_service.infrastructure.client.ai;
 
+import java.util.UUID;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import slack_service.infrastructure.client.ai.response.AiOutputTextForSlackV1;
+
+@FeignClient(
+    name = "aiClient",
+    url = "${clients.ai.url}",
+    configuration = slack_service.infrastructure.config.FeignConfig.class
+)
 public interface AiClient {
 
+    @PostMapping("/v1/ai/deadline/generate/{orderId}")
+    public AiOutputTextForSlackV1 generateDeadlineForSlack(@PathVariable UUID orderId);
 }
