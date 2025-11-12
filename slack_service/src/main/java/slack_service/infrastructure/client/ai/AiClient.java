51fbf3e3afb71e1426ef73c34460f3ac59557e84
Jess <jcscene@gmail.com>
2025-11-12 19:25:52 +0900

    [FIX] FeignClient name 수정

--- slack_service/src/main/java/slack_service/infrastructure/client/ai/AiClient.java
+++ slack_service/src/main/java/slack_service/infrastructure/client/ai/AiClient.java
@@ -4,15 +4,15 @@ import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import slack_service.infrastructure.client.ai.response.AiOutputTextForSlackV1;
+import slack_service.infrastructure.client.ai.response.AiDeadlineResponseV1;
 
 @FeignClient(
-    name = "aiClient",
+    name = "ai-service",
     url = "${clients.ai.url}",
     configuration = slack_service.infrastructure.config.FeignConfig.class
 )
 public interface AiClient {
 
     @PostMapping("/v1/ai/deadline/generate/{orderId}")
-    public AiOutputTextForSlackV1 generateDeadlineForSlack(@PathVariable UUID orderId);
+    AiDeadlineResponseV1 generateDeadlineByOrderId(@PathVariable("orderId") UUID orderId);
 }
