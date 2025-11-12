80d04fc8ec8027a76c4f8cb3792058f5c3ae0a38
mathos6147 <kgw6147@naver.com>
2025-11-13 00:09:11 +0900

    [FEAT] 허브 알고리즘에 필요한 adaptor 구현

--- hub_service/src/main/java/hub_server/hub_server/infra/repository/HubRouteLogRepositoryAdaptor.java
+++ hub_service/src/main/java/hub_server/hub_server/infra/repository/HubRouteLogRepositoryAdaptor.java
@@ -1,6 +1,59 @@
 package hub_server.hub_server.infra.repository;
 
+import hub_server.hub_server.domain.entity.HubRouteLog;
 import hub_server.hub_server.domain.repository.HubRouteLogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
 public class HubRouteLogRepositoryAdaptor implements HubRouteLogRepository {
+
+    private final JpaHubRouteLogRepository jpaHubRouteLogRepository;
+
+    @Override
+    public HubRouteLog save(HubRouteLog routeLog) {
+        return jpaHubRouteLogRepository.save(routeLog);
+    }
+
+    @Override
+    public Optional<HubRouteLog> findById(UUID id) {
+        return jpaHubRouteLogRepository.findById(id)
+                .filter(routeLog -> routeLog.getDeletedAt() == null);
+    }
+
+    @Override
+    public List<HubRouteLog> findAllActive() {
+        return jpaHubRouteLogRepository.findAllActive();
+    }
+
+    @Override
+    public Optional<HubRouteLog> findByStartAndEndHubWithStops(UUID startHubId, UUID endHubId) {
+        return jpaHubRouteLogRepository.findByStartAndEndHubWithStops(startHubId, endHubId);
+    }
+
+    @Override
+    public Optional<HubRouteLog> findByStartAndEndHub(UUID startHubId, UUID endHubId) {
+        return jpaHubRouteLogRepository.findByStartAndEndHub(startHubId, endHubId);
+    }
+
+    @Override
+    public void softDeleteByHubId(UUID hubId, UUID deletedBy) {
+        jpaHubRouteLogRepository.softDeleteByHubId(hubId, deletedBy);
+    }
+
+    @Override
+    public void softDeleteAll(UUID deletedBy) {
+        jpaHubRouteLogRepository.softDeleteAll(deletedBy);
+    }
+
+    @Override
+    public void softDeleteAll(){
+
+        jpaHubRouteLogRepository.softDeleteAll();
+    }
 }
